51b7b82aeaaf71d49ac0e77d64e83e20bd3a222b
Marco <argus.technologyit@gmail.com>
2024-08-21 11:39:33 +0200

    modificado los 2 section
    hemos añadido un efecto de autocompletado y hemos cambiado el fondo del primer section

--- src/script.js
+++ src/script.js
@@ -42,6 +42,33 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+//animacion del titulo de section 2
+
+const textElement = document.querySelector('.animate-text');
+const texts = [
+    "Proyectos impulsados por pasión y dedicación...",
+    "Juntos, damos vida a tus ideas con soluciones innovadoras..."
+];
+
+let index = 0;
+
+function typeText() {
+    textElement.textContent = texts[index];
+    textElement.style.width = '0'; // Resetea la animación
+    textElement.style.animation = 'none'; // Detiene la animación
+    setTimeout(() => {
+        textElement.style.animation = ''; // Reactiva la animación
+    }, 10);
+    index = (index + 1) % texts.length; // Cambia al siguiente texto
+}
+
+setInterval(typeText, 6000); // Cambia el texto cada 5 segundos
+
+// Inicializa con el primer texto
+typeText();
+
+
+
 
 
 
